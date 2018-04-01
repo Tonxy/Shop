c01b48a895301f2efad7887ba38b2a3604279963
wuyan_klay <wuyan_klay@163.com>
2018-04-01 14:00:11 +0800

    test stash

--- src/Test/testGit.java
+++ src/Test/testGit.java
@@ -5,5 +5,6 @@ public class testGit {
     public void testVersionControl(){
         System.out.println("test git version control");
         System.out.println("test version RollBack");
+        System.out.println("test git stash");
     }
 }
