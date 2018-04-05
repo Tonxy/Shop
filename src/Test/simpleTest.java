832747792ed9945af9ee78778e25ce8a99a2bff1
wuyan_klay <wuyan_klay@163.com>
2018-04-05 12:01:03 +0800

    test git stash

--- src/Test/simpleTest.java
+++ src/Test/simpleTest.java
@@ -5,4 +5,13 @@ public class simpleTest {
     public void output(){
         System.out.println("simpleTest Junit");
     }
+
+    public float add(float x,float y){
+       return x+y;
+    }
+    @Test
+    public void outputAdd(){
+        System.out.println("hh");
+        System.out.println(add(1,2));
+    }
 }
